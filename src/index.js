a45e8810a1dad4a6a45958c3c34ec97420defa20
Maria White <redstone.mari@gmail.com>
2022-07-23 15:12:46 +0300

    feat: solve morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = [];
+    for (let i = 0; i < Math.ceil(expr.length/10); i++){
+      letters[i] = expr.slice((i*10), (i*10) + 10);
+    }
+
+    letters.map((letter, index, arr) => {
+        if (letter.includes('*')) {
+            return arr[index] = ' ';
+        }
+        let str = letter.slice(letter.indexOf('1'));
+        let res = '';
+        for (let i = 0; i < str.length; i += 2) {
+            let sign = str.slice(i, i + 2);
+            res += (sign == '10' ? '.' : sign == '00' ? '' : '-');
+        }
+        return arr[index] = res;
+    });
+  
+    return letters.map(letter => {
+        if (letter === ' ') {
+            return ' ';
+        }
+        for (let key in MORSE_TABLE) {
+            if (key === letter) {
+                return MORSE_TABLE[key];
+            }
+        }
+    }).join('');
 }
 
 module.exports = {
